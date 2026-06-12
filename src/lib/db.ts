adadd1baf9bbbea82a61ffb81d9df968308a8c63
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-06-12 21:41:12 +0530

    mongoDb connection

---
+++ src/lib/db.ts
@@ -0,0 +1,36 @@
+const mongodbUrl= process.env.MONGO_URL
+import mongoose from "mongoose";
+
+// mongoDB url is not found 
+if(!mongodbUrl){
+    throw new Error("db url not found")
+}
+
+// Create a cache for like when user already exist not create any connection if user new then create connetion
+// Not multiple connection
+let cached = global.mongooseConn
+if(!cached){
+    cached=global.mongooseConn={conn:null,promise:null}
+}
+
+const connectDb= async () => {
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.promise){
+        cached.promise=mongoose.connect(mongodbUrl).then(c=>c.connection)
+    }
+
+    try{
+        const conn=await cached.promise
+        cached.conn=conn
+        return conn
+    }
+    catch(error){
+        console.log(error)
+        throw(error)
+    }
+}
+
+export default connectDb
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,7 +5,7 @@ interface IUser extends Document{
     email:string;
     password:string;
     createdAt:Date;
-    upatedAt:Date;
+    updatedAt:Date;
 }
 
 const userSchema= new mongoose.Schema<IUser>({
