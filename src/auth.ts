f2af438b288e84f92471d6a65f395db0eae2f705
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-06-12 23:46:33 +0530

    JWT auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,48 @@
-import NextAuth from "next-auth"
- 
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import connectDb from "./lib/db";
+import User from "./models/user.model";
+import bcrypt from "bcryptjs";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  providers: [
+    Credentials({
+      credentials: {
+        email: {
+          type: "email",
+          label: "Email",
+          placeholder: "johndoe@gmail.com",
+        },
+        password: {
+          type: "password",
+          label: "Password",
+          placeholder: "*****",
+        },
+      },
+      async authorize(credentials, request){
+        if(!credentials.email || !credentials.password){
+            throw Error("missing credentials")
+        }
+        const email = credentials.email;
+        const password = credentials.password as string
+        await connectDb()
+        const user = await User.findOne({email})
+
+        if(!user){
+            throw Error("user does not exist!")
+        }
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if(!isMatch){
+            throw Error("Incorrect Password")
+        }
+        return {
+            id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role
+        }
+      }
+    }),
+  ],
+});
