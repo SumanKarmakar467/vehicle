f7a5de17c2b905e03aec5043602e10f78cb2a7fe
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-06-14 23:24:02 +0530

    add

--- src/app/partner/onboarding/vehicle/page.tsx
+++ src/app/partner/onboarding/vehicle/page.tsx
@@ -15,7 +15,7 @@ const VEHICLES = [
 
 const Page = () => {
   const router = useRouter();
-  const [vehicleType, setVehicleType] = useState("");
+  const [vehicleType, setVehicleType] = usState("");
   const [vehicleNumber, setVehicleNumber] = useState("");
   const [vehicleModel, setVehicleModel] = useState("");
   
