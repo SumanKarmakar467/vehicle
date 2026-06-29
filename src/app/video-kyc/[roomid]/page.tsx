83ecdce90dfb91f27800545b45b8060624cc1298
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-06-29 23:18:29 +0530

    add

--- src/app/video-kyc/[roomid]/page.tsx
+++ src/app/video-kyc/[roomid]/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
   const [strem, setStream] = useState<MediaStream | null>(null);
 
   useEffect(() => {
-    if (joied) return;
+    if (jonied) return;
     let localstream: MediaStream;
     const init = async () => {
       try {
