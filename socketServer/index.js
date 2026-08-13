77a607a367fc1df0f3fea3c6063bb4e3c692d735
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-08-13 15:09:10 +0530

    add

--- socketServer/index.js
+++ socketServer/index.js
@@ -34,7 +34,7 @@ io.on("connection", (socket) => {
   socket.on("identity", async(userId) => {
     socket.userId=userId
     await User.findByIdAndUpdate(userId,{
-      socketId:socket.id,
+      socketId:ocket.id,
       isOnline:true
     })
 
