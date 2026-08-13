67362a18a5271065dba07f5b94741ea14c97f187
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-08-13 15:10:01 +0530

    add

--- socketServer/index.js
+++ socketServer/index.js
@@ -52,7 +52,7 @@ io.on("connection", (socket) => {
     await User.findByIdAndUpdate(userId,{
       location:{
         type:"Point",
-        coordinates:[longitude,latitude]
+        coordinates:[longitude,latiude]
       }
     })
   })
