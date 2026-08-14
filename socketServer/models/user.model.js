6add7e63f641e2734f2fe6a1d727b29222d947cd
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-08-14 20:59:31 +0530

    add

--- socketServer/models/user.model.js
+++ socketServer/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema= new mongoose.Schema({
     role:{
         type:String,
         default:"user",
-        enum:["user","partner","admin"]
+        enum:["user","partnr","admin"]
     },
     isEmailVerified:{
         type:Boolean,
