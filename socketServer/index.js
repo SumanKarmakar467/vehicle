da7cb2f7036ac9d7be48edd80d2a573c73f250af
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-08-20 21:42:25 +0530

    a

--- socketServer/index.js
+++ socketServer/index.js
@@ -42,7 +42,7 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect",async() => {
     if(!socket.userId) return;
-    await User.findByIdAndUpdate(socket.userId,{
+    await User.findByIAndUpdate(socket.userId,{
       socketId:null,
       isOnline:false
     })
