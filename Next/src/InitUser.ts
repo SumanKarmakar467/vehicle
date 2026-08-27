7c6e6e3c82bfc8583454699d6969d10f5aed113c
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-08-27 21:29:17 +0530

    add

--- Next/src/InitUser.ts
+++ Next/src/InitUser.ts
@@ -6,7 +6,7 @@ import useGetMe from "./hooks/useGetMe";
 function InitUser() {
   const { status } = useSession();
 
-  useGetMe(status === "authnticated");
+  useGetMe(status === "authenticated");
 
   return null;
 }
